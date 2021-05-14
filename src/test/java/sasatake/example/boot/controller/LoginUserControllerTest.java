f1212832975cd6d4be5fe460ca77524908450736
sasatake <t06418as@gmail.com>
2021-05-15 08:26:53 +0900

    add controller test

--- src/test/java/sasatake/example/boot/controller/LoginUserControllerTest.java
+++ src/test/java/sasatake/example/boot/controller/LoginUserControllerTest.java
@@ -3,6 +3,7 @@ package sasatake.example.boot.controller;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlTemplate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,4 +26,33 @@ public class LoginUserControllerTest {
     mvc.perform(MockMvcRequestBuilders.get("/users").accept(MediaType.TEXT_HTML_VALUE)).andExpect(status().isOk())
         .andExpect(view().name("user/list")).andExpect(model().attributeExists("userList"));
   }
+
+  @Test
+  public void registerInput() throws Exception {
+
+    mvc.perform(MockMvcRequestBuilders.get("/user/register/input").accept(MediaType.TEXT_HTML_VALUE))
+        .andExpect(status().isOk()).andExpect(view().name("user/input")).andExpect(model().attributeExists("form"));
+  }
+
+  @Test
+  public void registerConfirm() throws Exception {
+
+    mvc.perform(MockMvcRequestBuilders.post("/user/register/confirm").accept(MediaType.TEXT_HTML_VALUE))
+        .andExpect(status().isOk()).andExpect(view().name("user/confirm")).andExpect(model().attributeExists("form"));
+  }
+
+  @Test
+  public void register() throws Exception {
+
+    mvc.perform(MockMvcRequestBuilders.post("/user/register").accept(MediaType.TEXT_HTML_VALUE))
+        .andExpect(status().is3xxRedirection()).andExpect(redirectedUrlTemplate("/user/register/complete"));
+  }
+
+  @Test
+  public void registerComplete() throws Exception {
+
+    mvc.perform(MockMvcRequestBuilders.get("/user/register/complete").accept(MediaType.TEXT_HTML_VALUE))
+        .andExpect(status().isOk()).andExpect(view().name("user/complete"));
+  }
+
 }
